006c0a575eed697d48d95713b35c5e91bfe4b979
Helge <elitaer@gmail.com>
2023-12-23 14:42:23 +0100

    new plugin

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,31 +1,96 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver" }
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({})
-      lspconfig.lua_ls.setup({})
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
+        opts = {}
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            local on_attach = function(_, bufnr)
+                -- NOTE: Remember that lua is a real programming language, and as such it is possible
+                -- to define small helper and utility functions so you don't have to repeat yourself
+                -- many times.
+                --
+                -- In this case, we create a function that lets us more easily define mappings specific
+                -- for LSP related items. It sets the mode, buffer and description for us each time.
+                local nmap = function(keys, func, desc)
+                    if desc then
+                        desc = 'LSP: ' .. desc
+                    end
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+                    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+                end
+
+                nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+                nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+                nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+                nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+                nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+                nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+                nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+                nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+                -- See `:help K` for why this keymap
+                nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+                nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+                -- Lesser used LSP functionality
+                nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+                nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+                nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+                nmap('<leader>wl', function()
+                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                end, '[W]orkspace [L]ist Folders')
+
+                -- Create a command `:Format` local to the LSP buffer
+                vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+                    vim.lsp.buf.format()
+                end, { desc = 'Format current buffer with LSP' })
+            end
+
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+            local servers = {
+                lua_ls = {},
+                tsserver = {},
+                svelte = {},
+                jsonls = {},
+                tailwindcss = {},
+            }
+
+            local mason_lspconfig = require("mason-lspconfig")
+            mason_lspconfig.setup { ensure_installed = vim.tbl_keys(servers) }
+            mason_lspconfig.setup_handlers {
+                function(server_name)
+                    require('lspconfig')[server_name].setup {
+                        capabilities = capabilities,
+                        on_attach = on_attach,
+                        settings = servers[server_name],
+                        filetypes = (servers[server_name] or {}).filetypes,
+                    }
+                end,
+            }
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            --            local lspconfig = require("lspconfig")
+            --            lspconfig.tsserver.setup({})
+            --            lspconfig.lua_ls.setup({})
+            --            lspconfig.svelte.setup({})
+            --            lspconfig.tailwindcss.setup({})
+            --            lspconfig.jsonls.setup({})
+            --
+            --            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            --            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            --            vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
+            --            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+        end
+    }
 }
