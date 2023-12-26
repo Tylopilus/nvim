f25d092402ee9cbb146e8793bb89ce925575f0ac
Helge <elitaer@gmail.com>
2023-12-26 14:44:58 +0100

    config add

--- lua/plugins/project-nvim.lua
+++ lua/plugins/project-nvim.lua
@@ -5,7 +5,7 @@ return {
     require("project_nvim").setup {
       -- Manual mode doesn't automatically change your root directory, so you have
       -- the option to manually do so using `:ProjectRoot` command.
-      manual_mode = false,
+      manual_mode = true,
 
       -- Methods of detecting the root directory. **"lsp"** uses the native neovim
       -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
@@ -30,7 +30,7 @@ return {
 
       -- When set to false, you will get a message when project.nvim changes your
       -- directory.
-      silent_chdir = true,
+      silent_chdir = false,
 
       -- What scope to change the directory, valid options are
       -- * global (default)
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,5 +8,6 @@ return {
     local builtin = require("telescope.builtin")
     vim.keymap.set('n', '<C-p>', builtin.find_files, {})
     vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
   end
 }
