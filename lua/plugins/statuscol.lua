4e7924ebc76383dbe11434ad364c030a0a3af85a
Helge <elitaer@gmail.com>
2024-01-02 13:03:36 +0100

    adjusted statuscol

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -6,18 +6,27 @@ return {
             -- configuration goes here, for example:
             relculright = true,
             segments = {
-                { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
                 {
-                    sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
+                    sign = { name = { "Diagnostic" }, maxwidth = 1, auto = true },
                     click = "v:lua.ScSa",
                 },
+                {
+                    text = {builtin.foldfunc},
+                    click = "v:lua.ScFa"
+                },
                 { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
-                { text = { "%s" }, click = "v:lua.ScSa" },
+                -- { text = { "%s" }, click = "v:lua.ScSa" },
                 -- { text = { "%C" }, click = "v:lua.ScFa" },
-                -- {
-                --     sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
-                --     click = "v:lua.ScSa",
-                -- },
+                {
+                    sign = {
+                        text = { ".*" },
+                        -- maxwidth = 2,
+                        -- colwidth = 1,
+                        -- auto = true,
+                        -- wrap = true,
+                    },
+                    click = "v:lua.ScSa",
+                },
             },
         })
     end,
