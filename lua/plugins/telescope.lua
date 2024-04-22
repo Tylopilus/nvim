d5a62c9b93aac763caf231ef39c090211f688ab3
Helge <elitaer@gmail.com>
2024-04-22 13:21:44 +0200

    updated config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,31 +1,50 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    cond = not vim.g.vscode,
-    config = function()
-        require("telescope").setup({
-            defaults = {
-                layout_config = {
-                    width = 0.7,
-                    horizontal = {
-                        preview_width = 0.6,
-                    },
-                },
-            },
-            pickers = {
-                buffers = {
-                    ignore_current_buffer = true,
-                    sort_mru = true,
-                },
-            },
-        })
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>/", builtin.live_grep, {})
-        vim.keymap.set("n", "?", builtin.live_grep, {})
-        vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-    end,
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.5",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope-live-grep-args.nvim",
+	},
+	cond = not vim.g.vscode,
+	config = function()
+		local lga_actions = require("telescope-live-grep-args.actions")
+		require("telescope").setup({
+			defaults = {
+				layout_config = {
+					width = 0.7,
+					horizontal = {
+						preview_width = 0.6,
+					},
+				},
+			},
+			pickers = {
+				buffers = {
+					ignore_current_buffer = true,
+					sort_mru = true,
+				},
+			},
+			extensions = {
+				live_grep_args = {
+					auto_quoting = true, -- enable/disable auto-quoting
+					-- define mappings, e.g.
+					mappings = { -- extend mappings
+						i = {
+							["<C-k>"] = lga_actions.quote_prompt(),
+							["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+						},
+					},
+					-- ... also accepts theme settings, for example:
+					-- theme = "dropdown", -- use dropdown theme
+					-- theme = { }, -- use own theme spec
+					-- layout_config = { mirror=true }, -- mirror preview pane
+				},
+			},
+		})
+		require("telescope").load_extension("live_grep_args")
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>/", builtin.live_grep, {})
+		vim.keymap.set("n", "?", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", {})
+		vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+	end,
 }
