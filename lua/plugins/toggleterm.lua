2305d5a999c6a813c0c06b159d78077e1ca0967e
Helge <elitaer@gmail.com>
2024-05-02 20:40:34 +0200

    update toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,22 +1,23 @@
 return {
-	{
-		"akinsho/toggleterm.nvim",
-		cond = not vim.g.vscode,
-		version = "*",
-		config = function()
-			require("toggleterm").setup({
-				direction = "float",
-				on_open = function(term)
-					vim.cmd("startinsert!")
-					vim.api.nvim_buf_set_keymap(
-						term.bufnr,
-						"n",
-						"q",
-						"<cmd>close<CR>",
-						{ noremap = true, silent = true }
-					)
-				end,
-			})
-		end,
-	},
+  {
+    "akinsho/toggleterm.nvim",
+    cond = not vim.g.vscode,
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        direction = "float",
+        on_open = function(term)
+          vim.cmd("startinsert!")
+          vim.api.nvim_buf_set_keymap(
+            term.bufnr,
+            "n",
+            "q",
+            "<cmd>close<CR>",
+            { noremap = true, silent = true }
+          )
+        end,
+      })
+      vim.keymap.set("n", "<leader>tt", "<cmd>:ToggleTerm<cr>", {})
+    end,
+  },
 }
