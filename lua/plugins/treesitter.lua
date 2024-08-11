dcda7793d249df171a1dd0fc3f56e741c46c94e2
Helge Ahrens <helge@Helges-MacBook-Pro.local>
2024-08-11 21:02:57 +0200

    update config. use wezterm

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,15 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    cond = not vim.g.vscode,
-    config = function()
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-            auto_installed = true,
-            highlight = { enable = true },
-            inded = { enable = true },
-        })
-    end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  cond = not vim.g.vscode,
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+      auto_install = true,
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
