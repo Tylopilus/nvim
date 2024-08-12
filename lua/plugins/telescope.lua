fea74458bbc1dc31fe0e38e4bc2c36176ef127f2
Helge Ahrens <helge@Helges-MacBook-Pro.local>
2024-08-12 12:30:58 +0200

    revert stuff

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -45,7 +45,7 @@ return {
           -- define mappings, e.g.
           mappings = {    -- extend mappings
             i = {
-              ["<C-k>"] = lga_actions.quote_prompt({postfix = "--fixed-strings"}),
+              ["<C-k>"] = lga_actions.quote_prompt(),
               ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
             },
           },
