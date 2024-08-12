dc369c38cf893afeec5ad2a7c24676a472f9a9b7
Helge Ahrens <helge@Helges-MacBook-Pro.local>
2024-08-12 12:26:07 +0200

    turn of regex for find with ripgrep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -39,10 +39,13 @@ return {
       extensions = {
         live_grep_args = {
           auto_quoting = true, -- enable/disable auto-quoting
+          additional_args = {
+            "--fixed-strings"
+          },
           -- define mappings, e.g.
           mappings = {    -- extend mappings
             i = {
-              ["<C-k>"] = lga_actions.quote_prompt(),
+              ["<C-k>"] = lga_actions.quote_prompt({postfix = "--fixed-strings"}),
               ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
             },
           },
